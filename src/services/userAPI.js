5f84e50c8b3c03865b7a17b27606e3a7ebf722d1
tienlm <jordanluu1301@gmail.com>
2023-10-10 15:13:30 +0700

    init: initalize project

--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,52 +1,54 @@
-import fetcher from "./fetcher"
+import fetcher from "./fetcher";
 
 const userAPI = {
-    signup: (values) => {
-        return fetcher.post("/Users/signup", values)
-    },
+   signup: (values) => {
+      console.log(typeof values.password);
+      return fetcher.post("/Users/signup", values);
+   },
 
-    signin: (values) => {
-        return fetcher.post("/Users/signin", values)
-    },
+   signin: (values) => {
+      console.log(values);
+      return fetcher.post("/Users/signin", values);
+   },
 
-    facebookLogin: (facebookToken) => {
-        return fetcher.post("/Userss/facebooklogin", {
-            facebookToken
-        })
-    },
+   facebookLogin: (facebookToken) => {
+      return fetcher.post("/Userss/facebooklogin", {
+         facebookToken,
+      });
+   },
 
-    updateUser: (values) => {
-        return fetcher.put("/Users/editUser", values)
-    },
+   updateUser: (values) => {
+      return fetcher.put("/Users/editUser", values);
+   },
 
-    deleteUser: (id) => {
-        return fetcher.delete("/Users/deleteUser", {
-            params: {
-                id
-            }
-        })
-    },
+   deleteUser: (id) => {
+      return fetcher.delete("/Users/deleteUser", {
+         params: {
+            id,
+         },
+      });
+   },
 
-    getUsers: (keyword) => {
-        if (!keyword) keyword = null
-        return fetcher("/Users/getUser", {
-            params: {
-                keyword
-            }
-        })
-    },
+   getUsers: (keyword) => {
+      if (!keyword) keyword = null;
+      return fetcher("/Users/getUser", {
+         params: {
+            keyword,
+         },
+      });
+   },
 
-    getUserByProjectId: (idProject) => {
-        return fetcher("/Users/getUserByProjectId", {
-            params: {
-                idProject
-            }
-        })
-    },
+   getUserByProjectId: (idProject) => {
+      return fetcher("/Users/getUserByProjectId", {
+         params: {
+            idProject,
+         },
+      });
+   },
 
-    tesToken: () => {
-        return fetcher.post("/Users/TestToken")
-    }
-}
+   tesToken: () => {
+      return fetcher.post("/Users/TestToken");
+   },
+};
 
-export default userAPI
+export default userAPI;
